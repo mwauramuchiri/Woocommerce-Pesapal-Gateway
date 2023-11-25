90577ee99be9b9354b9a66cf16d8f15eabb9d501
mwauramuchiri <gramwauu@gmail.com>
2023-11-25 19:17:49 +0300

    wordpress lint

--- includes/pesapal-order-pay.php
+++ includes/pesapal-order-pay.php
@@ -2,21 +2,21 @@
 
 defined( 'ABSPATH' ) || exit;
 
-add_filter('woocommerce_locate_template', 'custom_payment_gateway_order_pay_template', 10, 3);
+add_filter( 'woocommerce_locate_template', 'custom_payment_gateway_order_pay_template', 10, 3 );
 
-function custom_payment_gateway_order_pay_template($template, $template_name, $template_path) {
+function custom_payment_gateway_order_pay_template( $template, $template_name, $template_path ) {
   
-  if ($template_name === 'checkout/form-pay.php') {
+  if ( $template_name === 'checkout/form-pay.php' ) {
     $order_id = wc_get_order_id_by_order_key( $_GET['key'] );
     $order    = wc_get_order( $order_id );
 
-    if ('pesapal' !== $order->get_payment_method()) {
+    if ( 'pesapal' !== $order->get_payment_method() ) {
       return $template;
     }
 
     // Check if your custom template file exists in your plugin directory
     $custom_template = plugin_dir_path(__FILE__) . '../templates/checkout/order-pay.php';
-    if (file_exists($custom_template)) {
+    if (file_exists( $custom_template ) ) {
       return $custom_template;
     }
   }
