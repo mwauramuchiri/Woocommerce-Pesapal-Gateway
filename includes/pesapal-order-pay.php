0b1023416c589db3583cc333ec3687447930d127
mwauramuchiri <gramwauu@gmail.com>
2023-11-25 19:16:17 +0300

    no need for a custom order-received.php

--- includes/pesapal-order-pay.php
+++ includes/pesapal-order-pay.php
@@ -21,23 +21,6 @@ function custom_payment_gateway_order_pay_template($template, $template_name, $t
     }
   }
 
-  if ($template_name === 'checkout/order-received.php') {
-    global $wp;
-
-    $order_id = $wp->query_vars['order-received'];
-    $order    = wc_get_order( $order_id );
-
-    if ('pesapal' !== $order->get_payment_method()) {
-      return $template;
-    }
-
-    // Check if your custom template file exists in your plugin directory
-    $custom_template = plugin_dir_path(__FILE__) . '../templates/checkout/order-received.php';
-    if (file_exists($custom_template)) {
-      return $custom_template;
-    }
-  }
-
   return $template;
 }
 
